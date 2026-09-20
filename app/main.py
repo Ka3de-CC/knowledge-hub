142b1dbbdd006006e419f7489b771a394904bda7
Kaede <15858885+ka3de@user.noreply.gitee.com>
2026-09-20 14:23:56 +0800

    FastAPI  usage method --.get()

--- app/main.py
+++ app/main.py
@@ -1,6 +1,39 @@
-def hello():
-    return "Hello Knowledge Hub"
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return{
+        "message" : "Hello Knowledge Hub"
+    }
+
+@app.get("/hello/{name}")
+def hello(name):
+    return{
+        # f-string （格式化字符串） 将花括号中的变量值插入字符串
+        "message" : f"Hello {name}"
+    }
+
+@app.get("/search")
+def search(keyword: str):
+    return{
+        "keyword" : keyword
+    }
+
+@app.get("/user/{username}")
+def user(username):
+    return{
+        "username" : f"{username}"
+    }
+
+@app.get("/items")
+# 声明变量的类型
+def search(page : int,size : int):
+    return{
+        "page" : page,
+        "size" : size
+    }
 
 if __name__ == "__main__":
     print(hello())
-    
